b8695d037298f33703669f9bfebd5c97df0f1093
KKKK <496007818@qq.com>
2020-02-10 11:05:51 +0800

    add:添加ab的资源加载方式

---
+++ Assets/Framework/Scripts/Managers/Ressource/AssetBundleResourceLoader.cs
@@ -0,0 +1,48 @@
+﻿using ANFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleResourceLoader : IReousrceLoader
+{
+    private Dictionary<string, AssetBundle> m_LoadAssetBundleDict = new Dictionary<string, AssetBundle>();
+    private Dictionary<string,Object> m_LoadObjectDict = new Dictionary<string, Object>();
+
+    public void Init()
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Destroy()
+    {
+
+    }
+
+    public T LoadResource<T>(string path) where T : Object
+    {
+        //判断是否已经加载
+        //加载依赖
+        //加载ab
+        //实例化
+    }
+
+    private bool LoadAssetBundle()
+    {
+        bool isSuccess = true;
+        
+
+
+        return isSuccess;
+    }
+
+}
